382c31bb969a1eea25311f64c082f0b70386df7a
--replace-all <stanley.wei.04@gmail.com>
2024-09-25 18:27:53 -0700

    Added PNG/transparency compatibility

--- ImageToSTL/image_to_stl.py
+++ ImageToSTL/image_to_stl.py
@@ -27,32 +27,34 @@ if __name__ == "__main__":
     parser.add_argument('--z-scale', type=float, nargs='?', const=1.0, default=1.0,
                     dest='z_scale', help='Z scale of generated model')
 
-    parser.add_argument('--ignore-zeroes', dest='ignore_zeroes', action='store_false',
-                        help='Treat zero-valued pixels as non-solid (Default: True)')
+    parser.add_argument('--keep-zeroes', dest='keep_zeroes', action='store_true',
+                        help='Retain zero-valued pixels within the generated mesh')
     
     args = parser.parse_args();
-    image = cv2.imread(args.image, cv2.IMREAD_GRAYSCALE)
+    image = cv2.imread(args.image, -1)
 
     base_height = args.base
 
-    ignore_zeroes = args.ignore_zeroes
-    if ignore_zeroes:
-        ret, contours_mask = cv2.threshold(image,1,255,cv2.THRESH_BINARY)
-        if not (contours_mask == 0).any():
-            ignore_zeroes = False
-        base_height += 1
-    else:
-        contours_mask = np.ones(image.shape, image.dtype)
+    contours_mask = np.ones(image.shape[0:2], dtype=image.dtype)
+    if len(image.shape) == 3 and image.shape[2] == 4:
+        contours_mask[np.where(image[:, :, 3] == 0)] = 0
+
+    image = cv2.imread(args.image, cv2.IMREAD_GRAYSCALE)
+    keep_zeroes = args.keep_zeroes
+    if not keep_zeroes:
+        contours_mask[np.where(image == 0)] = 0
+        base_height -= 1
     contours = list(cv2.findContours(contours_mask, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)[0])
 
     # Top & Bottom
     top_bottom_faces = 4 * image.shape[0] * image.shape[1]  # Number of top/bottom faces
-    if ignore_zeroes:
+    if not keep_zeroes:
         top_bottom_faces -= 2 * np.count_nonzero(image == 0)
     meshed = mesh.Mesh(np.zeros(top_bottom_faces, dtype=mesh.Mesh.dtype))
 
     index = 0
-    if not ignore_zeroes:
+    has_holes = True if (contours_mask == 0).any() else False
+    if not has_holes:
         for i in range(image.shape[0] - 1):
             for j in range(image.shape[1] - 1):
                 meshed.vectors[index] = np.array([
@@ -80,10 +82,10 @@ if __name__ == "__main__":
         for i in range(image.shape[0] - 1):
             for j in range(image.shape[1] - 1):
                 corners = [
-                    image[i, j],
-                    image[i+1, j],
-                    image[i, j+1],
-                    image[i+1, j+1]
+                    contours_mask[i, j],
+                    contours_mask[i+1, j],
+                    contours_mask[i, j+1],
+                    contours_mask[i+1, j+1]
                 ]
                 if corners.count(0) > 1:
                     continue
